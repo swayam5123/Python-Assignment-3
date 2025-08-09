a1048c1cecd2176c1929895124d4521fe54edae3
swayam5123 <swayamshelar12@gmail.com>
2025-08-09 12:09:35 +0530

    Add files via upload

---
+++ Assignment 3 Task 2.py
@@ -0,0 +1,11 @@
+import math
+
+number = float(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+natural_log = math.log(number)
+sine_value = math.sin(number)
+
+print(f"\nSquare root of {number} is: {square_root}")
+print(f"Natural log of {number} is: {natural_log}")
+print(f"Sine value of {number} is: {sine_value}")
